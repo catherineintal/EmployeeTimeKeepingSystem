17b184472fc88a0823b1f93dea9ca00bb973bd06
catherineintal <catherineintal18@gmail.com>
2021-05-05 23:16:38 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EmployeeTimeKeepingSystem
 {
@@ -6,10 +7,7 @@ namespace EmployeeTimeKeepingSystem
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("=======================================");
             Console.WriteLine("      Employee Time Keeping System     ");
-            Console.WriteLine("======================================");
 
             Console.WriteLine();
 
@@ -37,7 +35,7 @@ namespace EmployeeTimeKeepingSystem
             Console.WriteLine("Inserted Successfully!");
 
             Console.WriteLine("======================================");
-          
+
             Console.WriteLine("Employee Name: " + firstName + lastName);
             Console.WriteLine("Date Today:  " + Date);
             Console.WriteLine("Time-In: " + TimeIn);
@@ -46,9 +44,20 @@ namespace EmployeeTimeKeepingSystem
 
             Console.WriteLine("======================================");
 
+            Console.WriteLine("  Employee Attendance Log in System   ");
+
+            var EmployeeAttendance = new List<string>()
+                {"05/02/21 - Time In: 8:00am - Time Out: 5:00pm",       "05/03/21 - Time In: 9:00am - Time Out: 6:00pm",
+                 "05/04/21 - Time In: 7:00am - Time Out: 7:00pm",       "05/05/21 - Time In: 10:00am - Time Out: 8:00pm" };
+
+            foreach (var AttendanceLog in EmployeeAttendance)
+            {
+                Console.WriteLine($"{AttendanceLog}");
 
 
-
+            }
         }
+
     }
 }
+
