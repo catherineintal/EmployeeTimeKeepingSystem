4e6d932d87ea20e4af795684d5b222782b6f2c3f
catherineintal <catherineintal18@gmail.com>
2021-05-11 16:21:16 +0700

    update list and method

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace EmployeeTimeKeepingSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("      Employee Time Keeping System     ");
+            Console.WriteLine("|----------------------------------------------|");
+            Console.WriteLine("|----------------------------------------------|");
+            Console.WriteLine("|         Employee Time Keeping System         |");
+            Console.WriteLine("|----------------------------------------------|");
+            Console.WriteLine("|----------------------------------------------|");
 
             Console.WriteLine();
 
@@ -18,10 +22,6 @@ namespace EmployeeTimeKeepingSystem
             string lastName;
             Console.Write("Enter your Last Name:   ");
             lastName = Console.ReadLine();
-
-            string id;
-            Console.Write("Enter your Employee ID:   ");
-            id = Console.ReadLine();
 
             string Date;
             Console.Write("Enter the date today:   ");
@@ -41,7 +41,6 @@ namespace EmployeeTimeKeepingSystem
             Console.WriteLine("======================================");
 
             Console.WriteLine("Employee Name: " + firstName + lastName);
-            Console.WriteLine("Employee ID:  " + id);
             Console.WriteLine("Date Today:  " + Date);
             Console.WriteLine("Time-In: " + TimeIn);
             Console.WriteLine("Time-Out: " + TimeOut);
@@ -49,20 +48,21 @@ namespace EmployeeTimeKeepingSystem
 
             Console.WriteLine("======================================");
 
-            Console.WriteLine("  Employee Attendance Log in System   ");
+                Console.WriteLine("  Employee Attendance  System   ");
 
-            var EmployeeAttendance = new List<string>()
+                var EmployeeAttendance = new List<string>()
                 {" 05/02/21 - Time In: 8:00am - Time Out: 5:00pm",       "05/03/21 - Time In: 9:00am - Time Out: 6:00pm",
                  " 05/04/21 - Time In: 7:00am - Time Out: 7:00pm",       "05/05/21 - Time In: 10:00am - Time Out: 8:00pm" };
 
-            foreach (var AttendanceLog in EmployeeAttendance)
-            {
-                Console.WriteLine($"{AttendanceLog}");
+                foreach (var AttendanceLog in EmployeeAttendance)
+                {
+                    Console.WriteLine($"{AttendanceLog}");
 
 
+                }
             }
-        }
 
+        }
     }
-}
+
 
