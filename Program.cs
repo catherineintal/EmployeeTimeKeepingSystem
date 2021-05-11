e48c37ed90136c28dfaecc3f8c6cc73b95a31b33
catherineintal <catherineintal18@gmail.com>
2021-05-11 14:31:01 +0700

    updated list and method

--- Program.cs
+++ Program.cs
@@ -19,6 +19,10 @@ namespace EmployeeTimeKeepingSystem
             Console.Write("Enter your Last Name:   ");
             lastName = Console.ReadLine();
 
+            string id;
+            Console.Write("Enter your Employee ID:   ");
+            id = Console.ReadLine();
+
             string Date;
             Console.Write("Enter the date today:   ");
             Date = Console.ReadLine();
@@ -37,6 +41,7 @@ namespace EmployeeTimeKeepingSystem
             Console.WriteLine("======================================");
 
             Console.WriteLine("Employee Name: " + firstName + lastName);
+            Console.WriteLine("Employee ID:  " + id);
             Console.WriteLine("Date Today:  " + Date);
             Console.WriteLine("Time-In: " + TimeIn);
             Console.WriteLine("Time-Out: " + TimeOut);
@@ -47,8 +52,8 @@ namespace EmployeeTimeKeepingSystem
             Console.WriteLine("  Employee Attendance Log in System   ");
 
             var EmployeeAttendance = new List<string>()
-                {"05/02/21 - Time In: 8:00am - Time Out: 5:00pm",       "05/03/21 - Time In: 9:00am - Time Out: 6:00pm",
-                 "05/04/21 - Time In: 7:00am - Time Out: 7:00pm",       "05/05/21 - Time In: 10:00am - Time Out: 8:00pm" };
+                {" 05/02/21 - Time In: 8:00am - Time Out: 5:00pm",       "05/03/21 - Time In: 9:00am - Time Out: 6:00pm",
+                 " 05/04/21 - Time In: 7:00am - Time Out: 7:00pm",       "05/05/21 - Time In: 10:00am - Time Out: 8:00pm" };
 
             foreach (var AttendanceLog in EmployeeAttendance)
             {
